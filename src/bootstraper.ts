129aa1efd6ed18f02af9b831cab8d284d126b473
Oleg (NodeJS DB) <olegdbdev@hitech18.online>
2019-05-15 11:11:08 +0300

    resolve empty error object

--- src/bootstraper.ts
+++ src/bootstraper.ts
@@ -4,7 +4,7 @@ import config from '~/config';
 import logger from '~/logger';
 
 process.on('unhandledRejection', reason => {
-  logger.fatal({ error: reason }, 'Unhandled Rejection');
+  logger.fatal(reason, 'Unhandled Rejection');
   process.exit(1);
 });
 
